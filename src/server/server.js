c26a364e06c2bce2d5589037461e0f65a15ed401
peretzprogram <or17058@gmail.com>
2023-02-22 13:59:21 +0200

    update

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,30 @@
 const express = require("express")
 const app = express();
-const server = require('http').createServer(app).listen(8080);
-const io = require('socket.io').listen(server);
+const PORT = 1234;
+
+const io = require('socket.io')(5174);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('player_move', (data) => {
+    console.log(`received player_move event from player ${data.player_id}`);
+    // process player movement
+    // update game state
+    // broadcast game state to all players
+    socket.broadcast.emit('game_state', { /* game state */ });
+  });
+});
+
+app.listen(PORT, (err)=>{
+  if(!err) {
+    console.log('ok')
+  }
+  else{
+    console.warn('bad')
+  }
+})
